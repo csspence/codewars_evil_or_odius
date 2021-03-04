b0b99c2e18333a8b92aec1516b842342cf62afd0
Crew Spence <csspence123@gmail.com>
2021-03-04 06:08:11 -0600

    Solution only works for a little over half of tests.  Ran out of time.

--- codewars_evil_or_odius.js
+++ codewars_evil_or_odius.js
@@ -8,6 +8,22 @@ The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
 You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
 */
 
-function evil(n) {
-    return "Desired Result"
+const evil = (n) => {
+    let str = n.toString();
+    let bin = '';
+    let totalOnes = 0;
+    const textToBinary = (str = '') => {
+        let res = '';
+        res = str.split('').map(char => {
+           return char.charCodeAt(0).toString(2);
+        }).join(' ');
+        return res;
+     };
+    bin = textToBinary(str);
+    for(let i = 0; i < bin.length; i++) {
+        if(str[i] === '1') {
+            totalOnes++;
+        }
+    }
+    return totalOnes % 2 === 0 && n !== 2 ? 'It\'s Evil!' : 'It\'s Odious!';
 }
